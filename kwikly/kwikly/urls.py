3f886772af317b143e22a64449a4a22bfda9f949
Dewick <153211988+biodio332@users.noreply.github.com>
2024-11-04 17:56:41 +0800

    Add files via upload
    first commit

---
+++ kwikly/kwikly/urls.py
@@ -0,0 +1,16 @@
+
+# kwikly/urls.py
+from django.contrib import admin
+from django.urls import path
+from main import views  # Import the view from the 'main' app
+from . import settings
+from django.conf.urls.static import static
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),  # Map the root URL ('/') to the home view
+    path("login/", views.user_login, name="login"),
+    path("logout/", views.user_logout, name="logout"),
+    path("register/", views.user_register, name="register"),
+    path('account/', views.account_information, name='account'),
+    path('account/change_password/', views.change_password, name='change_password'),
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
