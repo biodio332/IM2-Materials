84d39cd25ef03193aaebd37f3cb141ab05f06508
kent <113046720+zy-ven@users.noreply.github.com>
2024-11-21 00:25:13 +0800

    minor changes cyhpthon

--- Kwikly/kwikly/cart/urls.py
+++ Kwikly/kwikly/cart/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'cart'
 
 urlpatterns = [
-   path('cart/summary/', views.cart_summary, name='cart_summary'),
+     path('cart/summary/', views.cart_summary, name='cart_summary'),
    path('add/',views.cart_add,name='cart_add'),
    path('delete/<int:product_id>/', views.cart_delete, name='cart_delete'),
    path('update/,',views.cart_update,name='cart_update'),
--- Kwikly/kwikly/kwikly/urls.py
+++ Kwikly/kwikly/kwikly/urls.py
@@ -1,10 +1,11 @@
 
 # kwikly/urls.py
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include 
 from main import views  # Import the view from the 'main' app
 from . import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.store_view, name='store'), 
     path('admin/', admin.site.urls),
@@ -14,11 +15,7 @@ urlpatterns = [
     path("register/", views.user_register, name="register"),
     path('account/', views.account_information, name='account'),
     path('account/change_password/', views.change_password, name='change_password'),
-<<<<<<< Updated upstream:kwikly/kwikly/urls.py
-    path('store/', views.store_view, name='store'),
-=======
     path('cart/',include('cart.urls')),
      path('cart/summary/', views.cart_summary, name='cart_summary'),
->>>>>>> Stashed changes:Kwikly/kwikly/kwikly/urls.py
     path('store/<int:store_id>/', views.store_detail, name='store_detail'),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
