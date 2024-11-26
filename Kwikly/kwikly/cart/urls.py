92b4c3a0fccd123d52a88d48168d0e1144055ad1
Ikanstie <uniskowt@gmail.com>
2024-11-27 00:52:59 +0800

    Added Quantity and Delete Item in cart

--- Kwikly/kwikly/cart/urls.py
+++ Kwikly/kwikly/cart/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
      path('cart/summary/', views.cart_summary, name='cart_summary'),
    path('add/',views.cart_add,name='cart_add'),
    path('delete/<int:product_id>/', views.cart_delete, name='cart_delete'),
-   path('update/,',views.cart_update,name='cart_update'),
+   path('update/<int:product_id>/', views.cart_update, name='cart_update'),
 ]
