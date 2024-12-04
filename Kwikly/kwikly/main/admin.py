f0e740047902c7be509b3902476a02302d0a0a0d
kent <113046720+zy-ven@users.noreply.github.com>
2024-12-05 00:20:19 +0800

    Bit of adjustments

--- Kwikly/kwikly/main/admin.py
+++ Kwikly/kwikly/main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Customer, Store, Transaction, Order, OrderProduct, Product, Category
+from .models import Customer, Store, Transaction, Order, OrderProduct, Product, Category, Ad
 
 
 admin.site.register(Store)
@@ -14,3 +14,7 @@ class CustomerAdmin(admin.ModelAdmin):
     list_display = ('username', 'name', 'contact', 'address', 'is_active')
     search_fields = ('username',)
     ordering = ('username',)
+    
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'image', 'link')
